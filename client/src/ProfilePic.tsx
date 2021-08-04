7c342c1340672c514b4ef89665015a0883630730
Alistair Quinn <alistairjoelquinn@gmail.com>
2021-08-04 17:13:22 +0200

    updates annoying jsxaa11y comfig"

--- client/src/ProfilePic.tsx
+++ client/src/ProfilePic.tsx
@@ -10,20 +10,24 @@ interface UserData {
 }
 
 const ProfilePic: React.FC<Props> = ({ userData, toggleModal }) => {
-    userData.image = userData.image || 'https://alsimageuniverse.s3.amazonaws.com/jhHC3lw0fMcoDXJFxNpnk_6iFWpR92aG.png';
-    const imageDefault = (e: React.SyntheticEvent<HTMLImageElement> ) => {
-        e.currentTarget.setAttribute('src', 'https://alsimageuniverse.s3.amazonaws.com/jhHC3lw0fMcoDXJFxNpnk_6iFWpR92aG.png');
+    const profileimage =
+        userData.image || 'https://alsimageuniverse.s3.amazonaws.com/jhHC3lw0fMcoDXJFxNpnk_6iFWpR92aG.png';
+
+    const imageDefault = (e: React.SyntheticEvent<HTMLImageElement>) => {
+        e.currentTarget.setAttribute(
+            'src',
+            'https://alsimageuniverse.s3.amazonaws.com/jhHC3lw0fMcoDXJFxNpnk_6iFWpR92aG.png',
+        );
     };
+
     return (
-        <div>
-            <img
-                onClick={toggleModal}
-                src={userData.image}
-                alt={`${userData.first} ${userData.last}`}
-                onError={(e) => imageDefault(e)}
-            />
-        </div>
+        <img
+            onClick={toggleModal}
+            src={profileimage}
+            alt={`${userData.first} ${userData.last}`}
+            onError={(e) => imageDefault(e)}
+        />
     );
-}
+};
 
 export default ProfilePic;
