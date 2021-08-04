2e123b44b2e1f97a773c6410e147869487e02192
alistairjoelquinn <alistairjoelquinn@gmail.com>
2021-08-04 10:34:34 +0200

    Finished part 1 with updates

--- client/src/Registration.tsx
+++ client/src/Registration.tsx
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import { Component } from 'react';
 import axios from 'axios';
 
 interface State {
@@ -23,10 +23,18 @@ export default class Registration extends Component<Props, State> {
     }
 
     handleChange(e: React.ChangeEvent<HTMLInputElement>) {
-        if (e.target.name === 'first' || 'last' || 'email' || 'password') {
-            this.setState({
-                [e.target.name]: e.target.value
-            }, () => console.log(this.state));
+        if (
+            e.target.name === 'first' ||
+            e.target.name === 'last' ||
+            e.target.name === 'email' ||
+            e.target.name === 'password'
+        ) {
+            this.setState(
+                {
+                    [e.target.name]: e.target.value,
+                },
+                () => console.log(this.state),
+            );
         }
     }
 
@@ -34,25 +42,29 @@ export default class Registration extends Component<Props, State> {
         event.preventDefault();
         axios
             .post('/register', this.state)
-            .then(res => {
+            .then((res) => {
                 if (res.data.success) {
-                    location.reload();
+                    window.location.reload();
                 } else {
                     this.setState({ error: true });
                 }
             })
-            .catch(console.log)
+            .catch(console.log);
     }
 
     render() {
+        const { error } = this.state;
+
         return (
             <form>
-                {this.state.error && (<h1>There was an error!</h1>)}
+                {error && <h1>There was an error!</h1>}
                 <input type="text" name="first" placeholder="First Name" onChange={this.handleChange} />
                 <input type="text" name="last" placeholder="Last Name" onChange={this.handleChange} />
                 <input type="text" name="email" placeholder="Email Address" onChange={this.handleChange} />
                 <input type="password" name="password" placeholder="Password" onChange={this.handleChange} />
-                <button onClick={this.handleSubmit}>Sign Up</button>
+                <button type="submit" onClick={this.handleSubmit}>
+                    Sign Up
+                </button>
             </form>
         );
     }
--- server/server.js
+++ server/server.js
@@ -1,47 +1,53 @@
-const express = require("express");
+const express = require('express');
+
 const app = express();
-const compression = require("compression");
-const path = require("path");
-const { hash } = require('./auth');
-const { newUser } = require("./database/db");
+const compression = require('compression');
+const path = require('path');
 const cookieSession = require('cookie-session');
+const { hash } = require('./auth');
+const { newUser } = require('./database/db');
+
 const cookieSessionMiddleware = cookieSession({
     secret: `I'm always angry.`,
-    maxAge: 1000 * 60 * 60 * 24 * 90
+    maxAge: 1000 * 60 * 60 * 24 * 90,
 });
 
 app.use(compression());
 app.use(cookieSessionMiddleware);
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "..", "client", "public")));
+app.use(express.static(path.join(__dirname, '..', 'client', 'public')));
 
-app.get('/user/id.json', function (req, res) {
+app.get('/user/id.json', (req, res) => {
     res.json({
-        userId: req.session.userId
+        userId: req.session.userId,
     });
 });
 
 app.post('/register', (req, res) => {
     const { first, last, email, password } = req.body;
-    hash(password).then((passHash) => {
-        newUser(first, last, email, passHash).then(({ rows }) => {
-            req.session.userId = rows[0].id;
-            res.json({ success: true });
-        }).catch((err) => {
+    hash(password)
+        .then((passHash) => {
+            newUser(first, last, email, passHash)
+                .then(({ rows }) => {
+                    req.session.userId = rows[0].id;
+                    res.json({ success: true });
+                })
+                .catch((err) => {
+                    console.log(err);
+                    res.json({ success: false });
+                });
+        })
+        .catch((err) => {
             console.log(err);
             res.json({ success: false });
         });
-    }).catch((err) => {
-        console.log(err);
-        res.json({ success: false });
-    });
 });
 
-app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "..", "client", "index.html"));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'client', 'index.html'));
 });
 
-app.listen(process.env.PORT || 3001, function () {
+app.listen(process.env.PORT || 3001, () => {
     console.log("I'm listening.");
 });
