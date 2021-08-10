a498c9cae185dc50ab35f6609609136dcd1bc1da
Alistair Quinn <alistairjoelquinn@gmail.com>
2021-08-10 14:36:25 +0200

    2 users render with names and images

--- client/src/FindPeople.tsx
+++ client/src/FindPeople.tsx
@@ -1,13 +1,21 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 const FindPeopleStyles = styled.div`
     font-size: 5rem;
     color: antiquewhite;
 `;
 
-const FindPeople = () => {
+const imageDefault = (e: React.SyntheticEvent<HTMLImageElement>) => {
+    e.currentTarget.setAttribute(
+        'src',
+        'https://alsimageuniverse.s3.amazonaws.com/jhHC3lw0fMcoDXJFxNpnk_6iFWpR92aG.png',
+    );
+};
+
+const FindPeople: React.FC = () => {
     const [users, setUsers] = useState([]);
 
     useEffect(() => {
@@ -19,10 +27,21 @@ const FindPeople = () => {
 
     return (
         <FindPeopleStyles>
-            <p>Find People</p>
             {users.map((user) => (
                 <div key={user.id}>
-                    {user.first} {user.last}
+                    <div>
+                        {user.first} {user.last}
+                    </div>
+                    <Link to={`/user/${user.id}`}>
+                        <img
+                            src={
+                                user.image ||
+                                'https://alsimageuniverse.s3.amazonaws.com/jhHC3lw0fMcoDXJFxNpnk_6iFWpR92aG.png'
+                            }
+                            alt={user.first}
+                            onError={(e) => imageDefault(e)}
+                        />
+                    </Link>
                 </div>
             ))}
         </FindPeopleStyles>
