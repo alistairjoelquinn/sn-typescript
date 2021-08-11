494007e5ac5e449d5a0ba873b396db591a372abc
Alistair Quinn <alistairjoelquinn@gmail.com>
2021-08-11 14:11:18 +0200

    useParams grabs other user id on page load

--- client/src/OtherProfile.tsx
+++ client/src/OtherProfile.tsx
@@ -1,7 +1,14 @@
-const OtherProfile = () => (
-    <div>
-        <h1>Other Profile for user</h1>
-    </div>
-);
+import { useParams } from 'react-router';
+
+const OtherProfile = () => {
+    const { id } = useParams<{ id: string }>();
+    console.log('id: ', id);
+
+    return (
+        <div>
+            <h1>Other Profile for user {id}</h1>
+        </div>
+    );
+};
 
 export default OtherProfile;
