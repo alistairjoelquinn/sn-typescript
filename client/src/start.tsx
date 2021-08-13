37ed69d6b16b4739776639ee5b2e16d51152cd0c
Alistair Quinn <alistairjoelquinn@gmail.com>
2021-08-13 08:16:04 +0200

    adds start.js redux setup code

--- client/src/start.tsx
+++ client/src/start.tsx
@@ -1,12 +1,25 @@
 import axios from 'axios';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import * as immutableState from 'redux-immutable-state-invariant';
+import { Provider } from 'react-redux';
+
+import { reducer } from './redux/reducer';
 import App from './App';
 import Welcome from './Welcome';
+
+const store = createStore(reducer, applyMiddleware(immutableState.default()));
 
 axios.get('/auth/user/id.json').then(({ data }) => {
     if (!data.userId) {
         ReactDOM.render(<Welcome />, document.querySelector('main'));
     } else {
-        ReactDOM.render(<App />, document.querySelector('main'));
+        const elem = (
+            <Provider store={store}>
+                <App />
+            </Provider>
+        );
+
+        ReactDOM.render(elem, document.querySelector('main'));
     }
 });
