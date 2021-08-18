9a6d4ca40e2d8c9cfb197a0c5f52c8499996080c
Alistair Quinn <alistairjoelquinn@gmail.com>
2021-08-18 16:20:25 +0200

    adds formatted date to each of the chat messages

--- client/src/Chat.tsx
+++ client/src/Chat.tsx
@@ -1,4 +1,6 @@
 import styled from 'styled-components';
+import { formatRelative } from 'date-fns';
+
 import { useAppSelector } from './redux/hooks';
 import { socket } from './socket.io/socket';
 
@@ -25,6 +27,8 @@ const ChatStyles = styled.div`
     .chat-container {
         flex-grow: 3;
         overflow-y: scroll;
+        display: flex;
+        flex-direction: column-reverse;
         .single-message {
             display: flex;
             align-items: center;
@@ -35,11 +39,16 @@ const ChatStyles = styled.div`
                 padding: 1rem;
                 border-radius: 50%;
             }
+            p:nth-child(2) {
+                color: #9e9797;
+                font-size: 1rem;
+            }
         }
     }
     .input-container {
         flex-grow: 1;
         width: 100%;
+        min-height: 12vh;
         border-radius: 1rem;
     }
 `;
@@ -53,7 +62,10 @@ const Chat = () => {
                 {chatMessages.map((message) => (
                     <div className="single-message" key={message.commentId}>
                         <img src={message.image} alt={message.first} />
-                        <p key={message.commentId}>{message.comment}</p>
+                        <div>
+                            <p>{message.comment}</p>
+                            <p>{formatRelative(new Date(message.time), Date.now())}</p>
+                        </div>
                     </div>
                 ))}
             </div>
